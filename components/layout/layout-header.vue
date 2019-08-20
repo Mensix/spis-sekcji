7987236ce5dba33798c40947c08ee9fae5adc8da
Mensix <grzegorz.perun@gmail.com>
2019-08-20 10:08:28 +0200

    (tweak) Removed separators in header

--- components/layout/layout-header.vue
+++ components/layout/layout-header.vue
@@ -2,16 +2,11 @@
   <q-header>
     <q-toolbar class="bg-secondary text-white shadow-2">
       <i class="q-mr-sm fas fa-list" style="font-size: 21px;" />
-      <q-separator dark vertical inset />
       <q-toolbar-title>Spis Sekcji</q-toolbar-title>
       <q-space />
-      <q-separator dark vertical />
       <q-btn flat label="Sekcje" @click="$router.push('/')" />
-      <q-separator dark vertical />
       <q-btn flat label="Tag-grupki" @click="$router.push('/taggroups')" />
-      <q-separator dark vertical />
       <q-btn flat label="Hades" @click="$router.push('/dead')" />
-      <q-separator dark vertical />
     </q-toolbar>
   </q-header>
 </template>
