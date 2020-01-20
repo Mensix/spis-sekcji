a6f2dd4dc82fcd0172d6adc0f25e1d9fab8099af
Mensix <grzegorz.perun@gmail.com>
2020-01-20 11:52:12 +0100

    (tweak) Removed unnecessary component

--- components/layout/layout-info.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <q-banner class="bg-white text-black">
-    <template v-slot:avatar>
-      <i class="fas fa-info" style="font-size: 24px;"></i>
-    </template>
-    Tą tabelę można przewijać w lewo i prawo :)
-  </q-banner>
-</template>
