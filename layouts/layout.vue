abf18c47a75773fe466a3a9bde44b183928d6020
Mensix <grzegorz.perun@gmail.com>
2020-06-22 11:57:21 +0200

    (tweak) New submissions panel

--- layouts/layout.vue
+++ layouts/layout.vue
@@ -29,79 +29,6 @@
       <nuxt :keep-alive="!$nuxt.$route.path.match(/\/status/)" />
     </q-page-container>
     <layout-footer />
-    <q-dialog v-model="isFormDialogShown">
-      <q-card>
-        <q-card-section>
-          <div class="text-h6">
-            Formularz do zgłaszania brakujących grup
-          </div>
-          <q-separator class="q-mt-sm" />
-          <small v-if="wasFormSend === true" class="text-green"
-            >Dziękujemy! Twoja grupa zostanie dodana po aktualizacji spisu w
-            najbliższą niedzielę.</small
-          >
-          <q-form @submit="onSubmit" class="q-gutter-xs">
-            <q-select
-              v-model="form.groupType"
-              :options="['Sekcja', 'Tag-grupka']"
-              color="secondary"
-              options-selected-class="text-secondary"
-              label="Typ grupy"
-              options-dense
-            />
-            <q-input
-              v-model="form.name"
-              color="secondary"
-              label="Nazwa grupy"
-              required
-            >
-              <template v-slot:append>
-                <q-icon name="create" />
-              </template>
-            </q-input>
-            <q-input
-              v-model="form.link"
-              type="link"
-              color="secondary"
-              label="Link"
-              required
-            >
-              <template v-slot:append>
-                <q-icon name="link" />
-              </template>
-            </q-input>
-            <q-input
-              v-model="form.category"
-              :disable="form.groupType === 'Tag-grupka'"
-              color="secondary"
-              label="Kategoria"
-            >
-              <template v-slot:append>
-                <q-icon name="list" />
-              </template>
-            </q-input>
-            <q-input
-              v-model="form.keywords"
-              :disable="form.groupType === 'Tag-grupka'"
-              :rules="[value => validateKeywords(value)]"
-              color="secondary"
-              label="Słowa kluczowe"
-            />
-            <q-btn
-              class="full-width q-mb-md"
-              type="submit"
-              color="secondary"
-              label="Wyślij"
-            />
-          </q-form>
-          <q-separator />
-          <small
-            >Prosimy o niewysyłanie grup tajnych, ponieważ skrypt do
-            aktualizacji ich nie wykrywa.</small
-          >
-        </q-card-section>
-      </q-card>
-    </q-dialog>
   </q-layout>
 </template>
 
@@ -119,8 +46,6 @@ export default {
     return {
       message: '',
       updateStatus: {},
-      isFormDialogShown: false,
-      wasFormSend: false,
       form: {
         groupType: 'Sekcja',
         name: '',
@@ -161,41 +86,6 @@ export default {
         appId: '1:752464608547:web:7786ca37c8ae1dd0'
       }
       firebase.initializeApp(firebaseConfig)
-    }
-  },
-  methods: {
-    onSubmit() {
-      firebase
-        .database()
-        .ref(
-          `/submissions/${
-            this.form.groupType === 'Sekcja' ? 'sections' : 'taggroups'
-          }`
-        )
-        .push({
-          name: this.form.name,
-          link: this.form.link,
-          category: this.form.groupType === 'Sekcja' && this.form.category,
-          keywords: this.form.groupType === 'Sekcja' && this.form.keywords
-        })
-
-      this.form.groupType = this.form.name = this.form.link = this.form.category = this.form.keywords =
-        ''
-      this.wasFormSend = true
-    },
-    validateKeywords(value) {
-      if (value.length === 0) {
-        return true
-      } else if (
-        value.length > 0 &&
-        [this.form.name, this.form.link, this.form.category]
-          .map(x => x.toLowerCase())
-          .every(x => !x.includes(value.toLowerCase()))
-      ) {
-        return true
-      } else {
-        return 'Słowa kluczowe nie mogą zawierać nazwy grupy, jej linku lub kategorii'
-      }
     }
   }
 }
