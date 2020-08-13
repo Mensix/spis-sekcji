2fa2589add41844ebbdb64b4667e6c8deeda8548
Mensix <grzegorz.perun@gmail.com>
2020-08-13 12:34:11 +0200

    (fix) Fixed formatting

--- components/layout/layout-bottom-mobile.vue
+++ components/layout/layout-bottom-mobile.vue
@@ -1,6 +1,6 @@
 <template>
   <q-footer class="bg-white text-secondary fixed-bottom">
-    <q-tabs dense shrink no-caps :mobile-arrows="false">
+    <q-tabs :mobile-arrows="false" dense shrink no-caps>
       <q-route-tab label="Sekcje" icon="vertical_split" to="/" exact />
       <q-route-tab label="Tag-grupki" icon="list" to="/taggroups" exact />
       <q-route-tab label="Hades" icon="label_off" to="/dead" exact />
